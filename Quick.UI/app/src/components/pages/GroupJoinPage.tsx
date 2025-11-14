bdaa7a4f14ad4e415162aa3306c88792b92be3e2
Максим Пурыхин <m.puryhin@solarlab.ru>
2025-11-14 18:27:15 +0300

    1

--- Quick.UI/app/src/components/pages/GroupJoinPage.tsx
+++ Quick.UI/app/src/components/pages/GroupJoinPage.tsx
@@ -98,7 +98,7 @@ export function GroupJoinPage() {
               isClearable
               loadOptions={loadGroups}
               classNames={{
-                control: () => "input-select"
+                control: (state) => state.isDisabled ? "input-select input-select-disabled" : "input-select"
               }}
               isDisabled={isGroupSelectDisabled}
               loadingMessage={() => "Поиск..."}
