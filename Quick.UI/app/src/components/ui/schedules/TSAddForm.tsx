caa36b35c88324fe3c4b0de9822d8e742f41ebc5
Максим Пурыхин <m.puryhin@solarlab.ru>
2025-11-15 11:22:05 +0300

    deploy

--- Quick.UI/app/src/components/ui/schedules/TSAddForm.tsx
+++ Quick.UI/app/src/components/ui/schedules/TSAddForm.tsx
@@ -1,47 +1,57 @@
+import { useState } from 'react';
 import { TimeField } from '@mui/x-date-pickers';
+import dayjs, { Dayjs } from 'dayjs';
 import styles from './TSAddForm.module.css';
 
 interface TSAddFormoPS {
-    onCreate(): Promise<void>
+    onCreate(from: Dayjs | null, to: Dayjs | null): Promise<void>
     onCancel(): void
 }
 
-export function TSAddForm({onCreate, onCancel}: TSAddFormoPS) {
-    
-    
-    return <>
-        <header className={styles.modalHeader}>
-            <h1 className={styles.modalTitle}>Добавить пару</h1>
-        </header>
-        <main className={styles.modalBody}>
-          <div className={styles.formGroup}>
-            <TimeField
-                label="Начало"
-                format="HH:mm"
-                onChange={}
-            />
-            <TimeField
-                label="Конец"
-                format="HH:mm"
-                onChange={}
-            />
-          </div>
+export function TSAddForm({ onCreate, onCancel }: TSAddFormoPS) {
 
-          <div className={styles.modalActions}>
-            <button 
-              className={`${styles.btn} ${styles.btnCancel}`}
-              onClick={onCancel}  
-            >
-              Отмена
-            </button>
-            <button
-              disabled={false}
-              className={`${styles.btn} ${styles.btnCreate}`}
-              onClick={() => onCreate()}
-            >
-              Добавить
-            </button>
-          </div>
-        </main>
-    </>;
+    const [from, setFrom] = useState<Dayjs | null>(null);
+    const [to, setTo] = useState<Dayjs | null>(null);
+
+    return (
+        <>
+            <header className={styles.modalHeader}>
+                <h1 className={styles.modalTitle}>Добавить пару</h1>
+            </header>
+
+            <main className={styles.modalBody}>
+                <div className={styles.formGroup}>
+                    <TimeField
+                        label="Начало"
+                        format="HH:mm"
+                        value={from}
+                        onChange={(newValue) => setFrom(newValue)}
+                    />
+                    <TimeField
+                        label="Конец"
+                        format="HH:mm"
+                        value={to}
+                        onChange={(newValue) => setTo(newValue)}
+                    />
+                </div>
+
+                <div className={styles.modalActions}>
+                    <button
+                        className={`${styles.btn} ${styles.btnCancel}`}
+                        onClick={onCancel}
+                    >
+                        Отмена
+                    </button>
+
+                    <button
+                        disabled={!from || !to}
+                        className={`${styles.btn} ${styles.btnCreate}`}
+                        onClick={() => onCreate(from, to)}
+                    >
+                        Добавить
+                    </button>
+                </div>
+            </main>
+        </>
+    );
 }
