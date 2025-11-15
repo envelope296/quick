973bab9f8fa6925e1346c2fb445cc8d7300d8953
Максим Пурыхин <m.puryhin@solarlab.ru>
2025-11-15 12:19:44 +0300

    Конфиг для запуска на GitHub

--- Quick.UI/app/vite.config.ts
+++ Quick.UI/app/vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr' 
 
+const isGitHubPages = process.env.GH_PAGES === 'true';
+
 export default defineConfig({
-  base: "/",
+  base: isGitHubPages ? '/quick/' : '/',
   plugins: [react(), svgr()],
   resolve: {
     alias: {
